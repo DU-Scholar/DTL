480f5d84d6b1af8a786ecff922c2b1375e0eea46
kazuki-komori <komokazu.nankai@gmail.com>
2020-07-12 18:23:02 +0900

    update

---
+++ components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="c-header">hoge</div>
+</template>
+<script>
+export default {}
+</script>
+<style lang="scss" scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="layout">
+    <Header />
     <Nuxt />
   </div>
 </template>
-
+<script>
+import Header from '../components/Header'
+export default {
+  components: {
+    Header,
+  },
+}
+</script>
 <style lang="scss">
 @import '@/assets/scss/mixins';
 html {
